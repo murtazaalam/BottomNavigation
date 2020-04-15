1efdef00e3e35044ebc1b6776e95f84a7a9b6e97
murtazaalam <34296103+murtazaalam@users.noreply.github.com>
2020-04-15 13:18:40 +0530

    latest code

--- app/src/main/java/com/example/bottomnavigation/FavouriteFragment.java
+++ app/src/main/java/com/example/bottomnavigation/FavouriteFragment.java
@@ -4,15 +4,19 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class FavouriteFragment extends Fragment {
+    private TextView textView;
 
     public FavouriteFragment() {
         // Required empty public constructor
@@ -23,6 +27,13 @@ public class FavouriteFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favourite, container, false);
+        View view = inflater.inflate(R.layout.fragment_favourite, container, false);
+        textView = (TextView) view.findViewById(R.id.textView);
+        Log.d("data--",Constant.userData);
+        if(!TextUtils.isEmpty(Constant.userData)){
+            textView.setText(Constant.userData);
+        }
+        return view;
     }
+
 }
